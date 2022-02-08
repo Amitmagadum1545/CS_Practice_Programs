70cd4e0d9d40edcf79a8a0a64b7003ce49c868f6
Amitmagadum1545 <amitmagadum1008@gmail.com>
2022-02-08 23:22:33 +0530

    ADDED: Practice Program

---
+++ ConsoleApp2/Addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Addition
+    {
+        public static void GetAddition()
+        {
+            Console.WriteLine("Enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine($"Addition of {a} and {b} is {c}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/Substraction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Substraction
+    {
+        public static void GetSubstraction()
+        {
+            Console.WriteLine("Enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int subOfTwo = num1 - num2;
+            Console.WriteLine($"Substraction of {num1} and {num2} is {subOfTwo}");
+            Console.ReadLine();
+        }
+    }
+}
